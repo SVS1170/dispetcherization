85143337750e3a44031caff642c0f61152ed58eb
SVS1170 <svs1170@gmail.com>
2023-02-27 03:20:47 +0300

    create primitive data transfer (with global variable)

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 import textwrap
 
 from aiohttp import web
-
+data = "0,0,0,0";
 def crc16(data):
         crc = 0xFFFF
         l = len(data)
@@ -25,7 +25,6 @@ def crc16(data):
         result = data + chr(crc % 256).encode('latin-1') + chr(crc // 256).encode('latin-1')
         return result
 
-
 async def intro(request: web.Request) -> web.StreamResponse:
     txt = textwrap.dedent(
         """\
@@ -44,17 +43,14 @@ async def intro(request: web.Request) -> web.StreamResponse:
     await resp.write(binary)
     return resp
 
-
 async def simple(request: web.Request) -> web.StreamResponse:
     return web.Response(text="Simple answer")
-
 
 async def change_body(request: web.Request) -> web.StreamResponse:
     resp = web.Response()
     resp.body = b"Body changed"
     resp.content_type = "text/plain"
     return resp
-
 
 async def hello(request: web.Request) -> web.StreamResponse:
     resp = web.StreamResponse()
@@ -73,10 +69,13 @@ async def input(request: web.Request) -> web.StreamResponse:
     name = request.match_info.get("name")
     b = str.encode(name)
     crc = crc16(b)
-    answer = ("recieved " + name + " " + str(crc)).encode("utf8")
+    global data
+    data = b
+    answer = (data)
+    print(data,"\n")
     resp.content_length = len(answer)
-    resp.content_type = "text/plain"
-    print("state=", name, str(crc))
+    resp.content_type = "text/html"
+    # print("state=", data, str(crc))
     await resp.prepare(request)
     await resp.write(answer)
     await resp.write_eof()
@@ -87,23 +86,32 @@ async def output(request: web.Request) -> web.StreamResponse:
     name = request.match_info.get("name")
     b = str.encode(name)
     crc = crc16(b)
-    string = name
-    # a = string.split(',')
-    # # print(a)
-    # lena = len(a)
-    # for i in range(lena):
-    #     e = a[1 - i].split(':')
-    #     print(e)
-    # d = a[1].split(':')
-    answer = ("000002,255,255,00").encode("utf8")
+    global data
+    a = data.split(','.encode())
+    # print(a)
+    lena = len(a)
+    print(lena)
+    c = f'{a[0]},{a[1]},{a[2]},{a[3]}'
+    c = c.replace('b', '')
+    c = c.replace('\'', '')
+    answer = (c).encode("utf8")
     resp.content_length = len(answer)
-    resp.content_type = "text/plain"
+    resp.content_type = "text/html"
     # print("state=", name, str(crc))
     await resp.prepare(request)
     await resp.write(answer)
     await resp.write_eof()
     return resp
-
+async def wmnk1(request) -> web.StreamResponse:
+    # a = await request.post()
+    a = await request.json()
+    writeToDb(a)
+    # print(a)
+        # bot.send_message("1668469043", "ПОЦЕЛУЙ МЕНЯ В ЗАДНИЦУ")
+    return web.Response(text="ok", content_type='text/html')
+# target="_blank"
+def writeToDb(data):
+    print(data)
 
 def init() -> web.Application:
     app = web.Application()
@@ -111,9 +119,10 @@ def init() -> web.Application:
     app.router.add_get("/simple", simple)
     app.router.add_get("/change_body", change_body)
     app.router.add_get("/input/{name}", input)
-    app.router.add_get("/input", output)
-    app.router.add_get("/output/{name}", output)
+    app.router.add_get("/input", input)
+    app.router.add_post("/test", wmnk1)
     app.router.add_get("/output", output)
+    app.router.add_get("/output/{name}", output)
     return app
 
 
